13a69e2fbd0dc20114d4055d50036f764c6daba6
Marcos Medina <markensi9@outlook.com>
2024-04-17 08:32:47 +0100

    Adapt class Password

---
+++ src/main/java/Builder.java
@@ -0,0 +1,6 @@
+public interface Builder {
+    void withLength(int size);
+    void withLowercase(boolean lowercase);
+    void withUppercase(boolean uppercase);
+    void withNumber(boolean )
+}
--- src/test/java/PasswordTest.java
+++ src/test/java/PasswordTest.java
@@ -18,7 +18,35 @@ public class PasswordTest {
         String valid_password = "Aa345678_";
         String invalid_password = "kjfklds90fma";
         Password password = new Password();
-        assertTrue(password.is_valid(valid_password));
-        assertFalse(password.is_valid(invalid_password));
+        assertTrue(password.isValid(valid_password));
+        assertFalse(password.isValid(invalid_password));
+    }
+
+    @Test
+    void this_password_not_contain_numbers () {
+        String notValidPassword = "aaaAA_bvVbfc";
+        Password password = new Password();
+        assertFalse(password.isValid(notValidPassword));
+    }
+
+    @Test
+    void this_password_not_contain_lowercase () {
+        String notValidPassword = "AAAAAAA576385_";
+        Password password = new Password();
+        assertFalse(password.isValid(notValidPassword));
+    }
+
+    @Test
+    void this_password_not_contain_uppercase () {
+        String notValidPassword = "aaaaaa65747437___";
+        Password password = new Password();
+        assertFalse(password.isValid(notValidPassword));
+    }
+
+    @Test
+    void this_password_not_contain_underscore () {
+        String notValidPassword = "frtggEWVEWRG3453434";
+        Password password = new Password();
+        assertFalse(password.isValid(notValidPassword));
     }
 }
